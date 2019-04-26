34f4b0055c16832ebad9cc8c8ec9a51d003ed786
Yvo Cilon <yvo.cilon@appcore.nl>
2019-04-26 11:26:45 +0200

    add 'dark-mode'

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,19 @@ import { render } from "react-dom";
 import StoreProvider from "./store";
 import App from "./App";
 
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding:0;
+    margin:0;
+    background-color: #474343;
+  }
+`;
+
 render(
   <StoreProvider>
+    <GlobalStyle />
     <App />
   </StoreProvider>,
   document.getElementById("app")
